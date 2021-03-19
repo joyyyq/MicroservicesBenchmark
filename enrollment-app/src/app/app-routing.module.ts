7a82dfd6948e15eefb32fbe5ec515ee4f82fdeac
Joy Qi <joy_huanqi@outlook.com>
2021-03-19 17:18:16 -0500

    routing

--- enrollment-app/src/app/app-routing.module.ts
+++ enrollment-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
