2df1f61251ed424c7e6c6f0b83a34798dffdaa01
Tamzid Ahmed <54724776+ta326@users.noreply.github.com>
2021-03-26 03:28:41 -0400

    Update register.component.ts

--- enrollment-app/src/app/register/register.component.ts
+++ enrollment-app/src/app/register/register.component.ts
@@ -8,6 +8,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup; 
+  loading = false;
+  submitted = false;
+  returnUrl?: string;
   constructor(private formBuilder: FormBuilder,) { 
     this.registerForm = this.formBuilder.group({
         username: ['', Validators.required],
@@ -16,8 +19,32 @@ export class RegisterComponent implements OnInit {
         password: ['', Validators.required]
     });
   }
-
+// convenience getter for easy access to form fields
+get f() { return this.registerForm!.controls; }
   ngOnInit(): void {
   }
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    // this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.registerForm!.invalid) {
+        return;
+    }
+
+    // this.loading = true;
+    // this.authenticationService.login(this.f.username.value, this.f.password.value)
+    //     .pipe(first())
+    //     .subscribe(
+    //         data => {
+    //             this.router.navigate([this.returnUrl]);
+    //         },
+    //         error => {
+    //             this.alertService.error(error);
+    //             this.loading = false;
+    //         });
+}
 
 }
