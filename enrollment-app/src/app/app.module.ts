b05b9908f3a9a94df7023c4d5a7d25a439a6fb6e
Anthony Ngoma <anthonyngoms6@gmail.com>
2021-04-16 19:15:12 -0500

    Beautify the navbar as well as the login page

--- enrollment-app/src/app/app.module.ts
+++ enrollment-app/src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// material 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';  
+import { MatSidenavModule } from '@angular/material/sidenav';  
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,6 +26,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatExpansionModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatSidenavModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatInputModule, 
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatBadgeModule, 
+    MatCardModule
   ],
   providers: [
   ],
