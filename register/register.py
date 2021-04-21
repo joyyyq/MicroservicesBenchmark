78caa42637c61eeda16d073ab1590c8a06f76072
Tamzid Ahmed <ta326@cornell.edu>
2021-04-20 18:55:26 -0700

    added wishlist and cart databse, modified register.py to add wishlist and shopping cart when student sign up

--- register/register.py
+++ register/register.py
@@ -13,7 +13,11 @@ from studentRegister_pb2 import (
 import studentRegister_pb2_grpc
 
 client = pymongo.MongoClient("register_db",27017)
+client_2 = pymongo.MongoClient("cart_db",27017)
+client_3 = pymongo.MongoClient("wishlist_db",27017)
 db = client.register
+db_2 = client_2.cart
+db_3 = client_3.wishlist
 
 class registerService(
     studentRegister_pb2_grpc.registerServicer
@@ -29,21 +33,25 @@ class registerService(
             return Response(success=True)
 
     def register(self, request, context):
+        #global client,db
         if( db.studentInfo.count_documents({"userName":request.userName}) > 0 ):
         # if( not db.studentInfo.find_one({"userName": request.userName}) is None ):
             print("failed")
             #context.abort(grpc.StatusCode.ALREADY_EXISTS, "user already exist")
             return Response(success=False)
-        #request = MessageToJson(request)
         salt = bcrypt.gensalt()
         hashed = bcrypt.hashpw(request.password.encode('utf-8'), salt)
-        request = {'userName':request.userName, 'password':hashed, 'firstName':request.firstName, 'lastName': request.lastName, }
+        request_1 = {'userName':request.userName, 'password':hashed, 'firstName':request.firstName, 'lastName': request.lastName, }
         print(request)
-        db.studentInfo.insert_one(request)
+        request_2 = {'userName':request.userName, 'cart':[],}
+        request_2 = {'userName':request.userName, 'wishlist':[],}
+        db_2.cartInfo.insert_one(request_2)
+        db_3.wishlistInfo.insert_one(request_3)
+        db.studentInfo.insert_one(request_1)
         return Response(success=True)
     
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=20))
     studentRegister_pb2_grpc. add_registerServicer_to_server(
         registerService(), server
     )
