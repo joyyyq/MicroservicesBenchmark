2f036f7dd20e5227ff576e7db8cabc8bcff39364
Anthony Ngoma <anthonyngoms6@gmail.com>
2021-04-21 23:00:08 -0500

    Added the classlist proto

--- classlist/class_manager.py
+++ classlist/class_manager.py
@@ -8,14 +8,61 @@ import urllib.request
 import time
 import requests
 import random
+from concurrent import futures
+
+import grpc
+
+from classList_pb2 import (
+    classListRequest,
+    classListResponse,
+    Class, 
+    Section, 
+    Time
+)
+import classList_pb2_grpc
+
+# client = pymongo.MongoClient("classlist_db",27017)
+client = pymongo.MongoClient("localhost",27018)
+db = client.classlist
+
+class classlistService(
+    classList_pb2_grpc.classlistServicer
+):
+    def getClassList(self, request, context):
+        classes = []
+        for class_ in db.classInfo.find({}):
+            temp_class = {}
+            temp_class['title'] = class_['titles'] 
+            temp_class['code'] = class_['course_code'] 
+            temp_class['subject'] = class_['subject'] 
+            temp_class['nbr'] = class_['nbr'] 
+            sections = [] 
+            for section_ in class_['course_info']:
+                temp_section = {}
+                temp_section['title'] = section_['section']
+                temp_section['number'] = section_['class_numbers']
+                temp_section['instructors'] = section_['instructors']
+                temp_section['days'] = section_['days']
+                times = section_['times'].split(" ")
+                temp_section['times'] = {'start': times[0], 'end': times[-1] }
+                sections.append(
+                    Section(title=temp_section['title'],number=temp_section['number'], 
+                    instructors=temp_section['instructors'], days=temp_section['days'], 
+                    times=temp_section['times']
+                ))
+
+            classes.append(Class(title=temp_class['title'],code=temp_class['code'], 
+                    subject=temp_class['subject'], nbr=temp_class['nbr'], 
+                    sections=sections
+                ))
+
+        return classListResponse(classes=classes)
 
 # loads only spring 21 classes
 TOTAL_ECE_CLASSES=0
 page = urllib.request.urlopen("https://classes.cornell.edu/browse/roster/SP21/subject/ECE")
 soup = BeautifulSoup(page,'html.parser')
 
-client = pymongo.MongoClient("classlist_db",27017)
-db = client.classlist
 titles = []
 instructors = []
 section = []
@@ -26,8 +73,6 @@ class_numbers = []    # unique number 34343
 credit = [] # 3 credits
 times = [] # 6-9
 days = [] # M-W
-
-
 
 def init_credits(): 
     # hard coding credits as I had difficulty grabbing the data
@@ -139,6 +184,15 @@ def update_db():
     # insert the total number of classes 
     db.classCounts.insert_one({'count': len(data_subject)})
 
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=20))
+    classList_pb2_grpc.add_classlistServicer_to_server(
+        classlistService(), server
+    )
+    server.add_insecure_port("0.0.0.0:5002")
+    server.start()
+    server.wait_for_termination()
+
 if __name__ == "__main__":
     TOTAL_ECE_CLASSES=scrape_num_class()
     if ( db.classCounts.count_documents({}) != TOTAL_ECE_CLASSES ):
@@ -148,5 +202,5 @@ if __name__ == "__main__":
         init_credits()
         scrape_classes()
         update_db()
-    while (True):
-        pass
+    
+    serve()
---
+++ classlist/client.py
@@ -0,0 +1,14 @@
+import grpc
+from classList_pb2_grpc import classlistStub
+channel = grpc.insecure_channel("localhost:5002")
+client = classlistStub(channel)
+from classList_pb2 import (
+    classListRequest,
+    classListResponse,
+    Class, 
+    Section, 
+    Time
+)
+request = classListRequest(year='')
+response = client.getClassList(request)
+print(response)
