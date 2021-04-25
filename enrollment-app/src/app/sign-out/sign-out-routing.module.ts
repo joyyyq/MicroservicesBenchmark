ec26870f546f425d6436822c4f467614261c3534
Tamzid Ahmed <ta326@cornell.edu>
2021-04-24 21:44:51 -0700

    added routing and module for sign out as they were ingonred before

---
+++ enrollment-app/src/app/sign-out/sign-out-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignOutComponent } from './sign-out/sign-out.component'; 
+
+const routes: Routes = [
+  {
+    path: '', 
+    component: SignOutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SignOutRoutingModule { }
