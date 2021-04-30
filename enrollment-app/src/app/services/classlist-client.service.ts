44d4d06e4102c9286acbdb268e9628ca95b317a8
Tamzid Ahmed <54724776+ta326@users.noreply.github.com>
2021-04-29 20:46:26 -0700

    fixed classlist update issue

--- enrollment-app/src/app/services/classlist-client.service.ts
+++ enrollment-app/src/app/services/classlist-client.service.ts
@@ -4,8 +4,9 @@ import { Injectable } from '@angular/core';
 
 import {classlistClient} from '../../../proto/ClassListServiceClientPb';
 
-import {Class, Section, Time, classListRequest, classListResponse} from '../../../proto/classList_pb';
-
+import {Class, Section, Time, classListRequest, classListResponse,} from '../../../proto/classList_pb';
+import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,17 +14,29 @@ export class ClasslistClientService {
 
   private client: classlistClient;
   constructor() {this.client = new classlistClient('http://localhost:8081');}
-
-  async getClassList(year: string) {
+  static addMessage(message: string, cssClass: string) {
+    //$('#first').after($('<div/>').addClass('row').append($('<h2/>').append(
+ //  $('<span/>').addClass('label ' + cssClass).text(message))));
+ }
+ static ERROR(message: string) {
+     this.addMessage(message, 'label-primary pull-left');
+ }
+  getClassList(year: string): BehaviorSubject<Class[]> {
     console.log("Classlist service");
     var request = new classListRequest; 
+    var result = new BehaviorSubject<Class[]>([]);
     request.setYear(year);
-    let result: any[] = [];
-    var response = this.client.getClassList(request,{'custom-header-1': 'value1'})
-    await response.then( (res) => {
-      console.log("Waiting ... ")
-      console.log(res)
-      console.log("Done")
-    })
+    this.client.getClassList(
+      request,{'custom-header-1': 'value1'},
+      ( err: grpcWeb.Error, response: classListResponse ) => {
+        if (err) {
+          ClasslistClientService.ERROR('Error code: ' + err.code + ' "' + err.message + '"');
+        }
+        result.next(response.getClassesList());
+        console.log("response is", result);
+      }
+    )
+    return result;
   }
 }
+
