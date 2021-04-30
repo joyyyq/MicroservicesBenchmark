5da4da295f53e0266aaff1dc5d42268345e2727b
Tamzid Ahmed <54724776+ta326@users.noreply.github.com>
2021-04-29 20:45:14 -0700

    Update roster.component.ts

--- enrollment-app/src/app/roster/roster/roster.component.ts
+++ enrollment-app/src/app/roster/roster/roster.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClasslistClientService } from '../../services/classlist-client.service';
+import {Class, Section, Time, classListRequest, classListResponse,} from '../../../../proto/classList_pb';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-roster',
@@ -7,11 +9,16 @@ import { ClasslistClientService } from '../../services/classlist-client.service'
   styleUrls: ['./roster.component.css']
 })
 export class RosterComponent implements OnInit {
+  public classList:Class[] = []
 
   constructor(private client: ClasslistClientService) { }
 
   ngOnInit(): void {
-    this.client.getClassList('SP21'); 
+    this.client.getClassList('SP21').asObservable().subscribe(val =>  {
+      this.classList = val;
+      console.log(this.classList);
+    }
+    )
   }
 
   call() {
