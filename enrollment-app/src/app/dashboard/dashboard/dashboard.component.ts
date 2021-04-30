a6d2113bd6954a795dda3fa8cc8ab150c21cca67
Joy Qi <joy_huanqi@outlook.com>
2021-04-29 22:54:44 -0500

    minor fix to find the new column courseID

--- enrollment-app/src/app/dashboard/dashboard/dashboard.component.ts
+++ enrollment-app/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 import { CartClientService } from '../../services/cart-client.service';
