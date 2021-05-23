52764dfd14fb5284ed5fca0b58e0a0637f9f02b3
Joy Qi <joy_huanqi@outlook.com>
2021-05-23 17:12:33 -0500

    add multi-thread timing

--- classlist/client.py
+++ classlist/client.py
@@ -1,4 +1,8 @@
 import grpc
+import time
+import threading
+import sys
+
 from classList_pb2_grpc import classlistStub
 channel = grpc.insecure_channel("localhost:5002")
 client = classlistStub(channel)
@@ -9,6 +13,32 @@ from classList_pb2 import (
     Section,
     Time,
 )
-request = classListRequest(year='')
-response = client.getClassList(request)
-print(response)
+num_requests = int(input('number of threads: '))
+print(num_requests)
+times = [0]*num_requests
+threads = []
+
+def get_response(thread_idx):
+    individual_start = time.time()
+    request = classListRequest(year='')
+    response = client.getClassList(request)
+    individual_end = time.time()
+    times[thread_idx] = individual_end - individual_start
+
+# try:
+start = time.time()
+for i in range(num_requests):
+    temp = threading.Thread(target=get_response, args=(i,))
+    threads.append(temp)
+    temp.start()
+# except:
+#     print('Error')
+
+for t in threads:
+    t.join()
+end = time.time()
+elapse = end - start
+print('start: ', start)
+print('end: ', end)
+print('elapse: ', elapse)
+print(times)
